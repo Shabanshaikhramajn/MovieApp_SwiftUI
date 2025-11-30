d22805f2aa5b5a81d69196682c76860938ac0d1e
shaban <shabanshaikh72345@gmail.com>
2025-11-30 17:33:11 +0530

    Initial Commit

---
+++ Xcode Demo/Xcode_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoApp.swift
+//  Xcode Demo
+//
+//  Created by shaban shaikh on 30/11/25.
+//
+
+import SwiftUI
+
+@main
+struct Xcode_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Xcode DemoTests/Xcode_DemoTests.swift
@@ -0,0 +1,17 @@
+//
+//  Xcode_DemoTests.swift
+//  Xcode DemoTests
+//
+//  Created by shaban shaikh on 30/11/25.
+//
+
+import Testing
+@testable import Xcode_Demo
+
+struct Xcode_DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
